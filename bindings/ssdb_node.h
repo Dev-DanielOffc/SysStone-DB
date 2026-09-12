0e766e8d6469754249f22f4c0a98f1524db4d050
Dev-DanielOffc <danielplacencio15@gmail.com>
2026-09-12 13:29:44 -0400

    Crear bindings/ssdb_node.h

---
+++ bindings/ssdb_node.h
@@ -0,0 +1,31 @@
+#ifndef SYSSTONE_NODE_H
+#define SYSSTONE_NODE_H
+
+#include <napi.h>
+#include <memory>
+#include "engine.h"
+
+namespace sysstone_node {
+
+class Database : public Napi::ObjectWrap<Database> {
+public:
+    static Napi::Object Init(Napi::Env env, Napi::Object exports);
+    explicit Database(const Napi::CallbackInfo& info);
+    ~Database();
+
+private:
+    std::unique_ptr<sysstone::Engine> engine_;
+
+    Napi::Value Open(const Napi::CallbackInfo& info);
+    Napi::Value Close(const Napi::CallbackInfo& info);
+    Napi::Value Put(const Napi::CallbackInfo& info);
+    Napi::Value Get(const Napi::CallbackInfo& info);
+    Napi::Value Remove(const Napi::CallbackInfo& info);
+    Napi::Value Keys(const Napi::CallbackInfo& info);
+    Napi::Value Find(const Napi::CallbackInfo& info);
+    Napi::Value Count(const Napi::CallbackInfo& info);
+};
+
+}
+
+#endif
